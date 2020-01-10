3cac692cc1a6fde18b2ad1b0c863938d07c2ba5b
Gaurav KC <13gaurab@gmail.com>
2020-01-10 10:21:07 +0545

    Feature/embeddedhtml (#12)
    * Add widget type form
    
    * Handle error image case

--- comp/client/src/components/Config/Canvas.jsx
+++ comp/client/src/components/Config/Canvas.jsx
@@ -1,30 +1,30 @@
-import React, {useState} from "react";
-import Modal from 'react-bootstrap/Modal'
-import GridLayout from 'react-grid-layout';
-import { connect } from 'react-redux';
-import { saveCanvas } from '../../action';
-import WindowForm from '../Forms/WindowForm';
-import {Link} from 'react-router-dom';
-import './index.css';
-import TextToHtml from '../TextToHtml/TextToHtml';
+import React, { useState } from "react";
+import Modal from "react-bootstrap/Modal";
+import GridLayout from "react-grid-layout";
+import { connect } from "react-redux";
+import { saveCanvas } from "../../action";
+import WindowForm from "../Forms/WindowForm";
+import { Link } from "react-router-dom";
+import "./index.css";
+import { createMarkup } from "../../utils";
+import TextToHtml from "../TextToHtml/TextToHtml";
 
 const Canvas = props => {
   const [blockModalShow, setBlockModalShow] = useState(false);
   const canvasId = props.match.params.id;
   const currentCanvas = props.canvasList.find(canvas => canvas.id === canvasId);
-  const { windows, layout, single } = currentCanvas;
-
-  const handleSave = (data) => {
-    const {title, content, type} = data;
-    const windowId = `${canvasId}window-${windows.length + 1}`
+  const { windows, single } = currentCanvas;
+  const handleSave = data => {
+    const { title, content, type } = data;
+    const windowId = `${canvasId}window-${windows.length + 1}`;
     const newWindow = {
       id: windowId,
       title,
       content,
       type,
-      layout: {i: windowId, x: 0, y: 0, w: 2, h: 2}
+      layout: { i: windowId, x: 0, y: 0, w: 4, h: 6 }
     };
-    const updatedCanvas = props.canvasList.map( canvas => {
+    const updatedCanvas = props.canvasList.map(canvas => {
       if (canvas.id === canvasId) {
         canvas.windows.push(newWindow);
       }
@@ -32,57 +32,86 @@ const Canvas = props => {
     });
     props.dispatch(saveCanvas(updatedCanvas));
     setBlockModalShow(false);
-  }
+  };
 
-  const handleLayoutChange = (layout) => {
-    const updatedWindows = windows.map( (window, index) => {
+  const handleLayoutChange = layout => {
+    const updatedWindows = windows.map((window, index) => {
       window.layout = layout[index];
       return window;
     });
-    const updatedCanvas = props.canvasList.map ( canvas => {
+    const updatedCanvas = props.canvasList.map(canvas => {
       if (canvas.id === canvasId) {
         canvas.windows = updatedWindows;
       }
       return canvas;
     });
-    props.dispatch(saveCanvas(updatedCanvas));    
-  }
+    props.dispatch(saveCanvas(updatedCanvas));
+  };
 
   const renderLayout = () => {
-    return single ? <div className="h-100 w-100">
-      <div className="h-100 w-100" key={window.id} data-grid={window.layout}>
-        {renderWindow(windows[0])}
-      </div>
-    </div> :
-    <GridLayout className="layout" cols={12} rowHeight={50} width={window.innerWidth - 30} isDraggable={true} containerPadding={[15, 15]} onLayoutChange={handleLayoutChange}>
-    { windows.map( (window) => {
-      return (
-        <div className="custom-grid-item" key={window.id} data-grid={window.layout}>
-          {renderWindow(window)}
+    return single ? (
+      <div className="h-100 w-100">
+        <div className="h-100 w-100" key={window.id} data-grid={window.layout}>
+          {renderWindow(windows[0])}
         </div>
-      )
-    })}
-  </GridLayout>
-  }
+      </div>
+    ) : (
+      <GridLayout
+        className="layout"
+        cols={12}
+        rowHeight={50}
+        width={window.innerWidth - 30}
+        isDraggable={true}
+        containerPadding={[15, 15]}
+        onLayoutChange={handleLayoutChange}
+      >
+        {windows.map(window => {
+          return (
+            <div
+              className="custom-grid-item"
+              key={window.id}
+              data-grid={window.layout}
+            >
+              {renderWindow(window)}
+            </div>
+          );
+        })}
+      </GridLayout>
+    );
+  };
 
-  const renderWindow = (window) => {
-    if (window.type === 'url') {
-      return (
-        <iframe className="window-iframe" src={window.content.url} />
-      )
+  const renderWindow = window => {
+    if (window.type === "url") {
+      return <iframe className="window-iframe" src={window.content.url} />;
     }
-    return(
-        <TextToHtml content={window.content}/>
-    )
-  }
+    if (window.type === "coverpage") {
+      return <TextToHtml content={window.content} />;
+    }
+    if (window.type === "widget") {
+      const widgetHtml = window.content.widgetHtml;
+      return (
+        <div
+          className="widget-container"
+          dangerouslySetInnerHTML={createMarkup(widgetHtml)}
+        ></div>
+      );
+    }
+  };
 
   return (
     <div className="container-fluid vh-100">
-      <Link to={'/config'}><i className="fa fa-arrow-left"></i></Link>
+      <Link to={"/config"}>
+        <i className="fa fa-arrow-left"></i>
+      </Link>
       <div className="canvas-edit-header d-flex justify-content-between">
         <h2>{currentCanvas.title}</h2>
       </div>
-      <button className="btn btn-primary create-window" onClick={() => setBlockModalShow(true)}>Create Window</button>
+      <button
+        className="btn btn-primary create-window"
+        onClick={() => setBlockModalShow(true)}
+      >
+        Create Window
+      </button>
       <div className="canvas-container h-100">
         {windows.length ? renderLayout() : null}
       </div>
@@ -97,14 +126,14 @@ const Canvas = props => {
           </Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <WindowForm handleSave={handleSave}/>
+          <WindowForm handleSave={handleSave} />
         </Modal.Body>
       </Modal>
     </div>
-  )
-}
+  );
+};
 
-const mapStateToProps = (store) => ({
+const mapStateToProps = store => ({
   canvasList: store.canvas.canvasList
 });
 
